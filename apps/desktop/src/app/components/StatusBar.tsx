99b121d83a6de4893104285604ebe16d4cd08053
vakot <vakotua.g@gmail.com>
2026-08-03 17:52:20 +0200

    fix(layout): correct status bar metadata path

--- apps/desktop/src/app/components/StatusBar.tsx
+++ apps/desktop/src/app/components/StatusBar.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 
-import packageJson from "../../../../../../package.json";
+import packageJson from "../../../../../package.json";
 
 export function StatusBar() {
   const { t } = useTranslation();
